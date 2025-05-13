44f2b181d8a0d055ec8d1005ea36209d4fd234e0
Yindee <123phoorin@gmail.com>
2025-05-13 22:10:16 +0700

    Add a project

---
+++ ai_eval.py
@@ -0,0 +1,13 @@
+from beyond_game_for_ai import *
+
+if __name__ == '__main__':
+  # training AI vs AI
+  player1 = Player("player1", exp_rate=0.3)
+
+  # randomness
+  player2 = Player("player2", exp_rate=1)
+
+  Game = TicTacToeGameAI(player1, player2)
+
+  print("Training AI vs Randomness")
+  Game.test(100)
